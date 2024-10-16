f5db5d47bc2322f86eee73f6e64a94c7d4f8fd7f
AllanNguyen <allandog06@gmail.com>
2024-10-15 20:06:50 -0700

    Changed basically everything

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,11 @@
 // Your own functions here
 //---------------------------------
 
-
-function sayHello() {
-    //do something
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+    }).catch((error) => {
+        // An error happened.
+    });
 }
-//sayHello();    //invoke function
